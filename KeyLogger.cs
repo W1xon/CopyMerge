2029e7711f8de76c929c4a76424dfa004b3b2d25
W1xon <vladimir16buzo@gmail.com>
2024-04-30 16:01:42 +0700

    No Autoran

---
+++ KeyLogger.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyMerge
+{
+    internal class KeyLogger
+    {
+        private static SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+        private static StringBuilder keyBuffer = new StringBuilder();
+        private static StringBuilder clipboard = new StringBuilder();
+        [DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(Int32 i);
+        public static async Task KeyChecker()
+        {
+            while (true)
+            {
+                await Task.Delay(1);
+                await semaphore.WaitAsync();
+                try
+                {
+
+                    for (int i = 16; i < 68; i++)
+                    {
+                        int state = GetAsyncKeyState(i);
+                        if (state != 0)
+                        {
+                            if (((Keys)i) == Keys.Control || ((Keys)i) == Keys.ControlKey || ((Keys)i) == Keys.LControlKey || ((Keys)i) == Keys.RControlKey)
+                                keyBuffer.Append("CTRL ");
+                            else if (((Keys)i) == Keys.Shift || ((Keys)i) == Keys.ShiftKey || ((Keys)i) == Keys.LShiftKey || ((Keys)i) == Keys.RShiftKey)
+                                keyBuffer.Append("SHIFT ");
+                            else if (((Keys)i) == Keys.C)
+                                keyBuffer.Append("C ");
+                            else if (((Keys)i) == Keys.V)
+                                keyBuffer.Append("V ");
+                        }
+                    }
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }
+        }
+
+        public static async Task KeyBufferClear()
+        {
+            while (true)
+            {
+                await Task.Delay(10);
+                await semaphore.WaitAsync();
+                try
+                {
+                    if (keyBuffer.Length > 0 && GetAsyncKeyState(Convert.ToInt32(Keys.ControlKey)) == 0)
+                    {
+                        CopyHandler();
+                        keyBuffer.Clear();
+                    }
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            }
+        }
+        private static void CopyHandler()
+        {
+            string separator = SetSeparator();
+            string[] key = keyBuffer.ToString().Split(' ');
+            if (key.Contains("CTRL") && key.Contains("C"))
+            {
+                if (key.Contains("SHIFT"))
+                {
+                    if (!string.IsNullOrWhiteSpace(Clipboard.GetText()))
+                    {
+                        clipboard.Append(separator + Clipboard.GetText());
+                        Clipboard.SetText(clipboard.ToString());
+                    }
+                }
+                else
+                {
+                    clipboard.Clear();
+                    clipboard.Append(Clipboard.GetText());
+                }
+            }
+        }
+        private static string SetSeparator()
+        {
+            switch (Properties.Settings.Default.Separator)
+            {
+                case "Enter":
+                    return "\n";
+
+                case "Space":
+                    return " ";
+
+                case ", ":
+                    return ", ";
+
+                case ". ":
+                    return ". ";
+            }
+            return "";
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using System.Reflection;
+using System.IO;
+namespace CopyMerge
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NotifyIcon notifyIcon;
+        private WindowState m_storedWindowState = WindowState.Normal;
+        private bool isInitializing = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetAutoLaunch(true, Assembly.GetEntryAssembly().Location);
+           
+                SetComboBoxValue();
+                isInitializing = false;
+                notifyIcon = new NotifyIcon();
+                notifyIcon.BalloonTipText = "Программа свернута в трей, вы можете продолжать ей пользоваться :)";
+                notifyIcon.BalloonTipTitle = "Go To tray";
+                notifyIcon.Text = "CopyMerge";
+            try
+            {
+                System.Windows.MessageBox.Show("e", "1");
+                notifyIcon.Icon = new System.Drawing.Icon("icon.ico");
+                System.Windows.MessageBox.Show("e", "2");
+                notifyIcon.Click += new EventHandler(NotifyIcon_Click);
+                System.Windows.MessageBox.Show("e", "3");
+                KeyLogger.KeyBufferClear();
+                System.Windows.MessageBox.Show("e", "4");
+                KeyLogger.KeyChecker();
+                System.Windows.MessageBox.Show("e", "5");
+                File.AppendAllText("log.txt", "MainWindow: Components Initialized\n");
+                System.Windows.MessageBox.Show("e", "6");
+
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText("log.txt", ex.Message);
+            }
+            System.Windows.MessageBox.Show("e", "Final");
+        }
+        private bool SetAutoLaunch(bool launch, string path)
+        {
+            string name = "CopyMerge";
+            string ExePath = path;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (launch)
+                {
+                    reg.SetValue(name, ExePath);
+                }
+                else
+                {
+                    reg.DeleteValue(name);
+                }
+                reg.Flush();
+                reg.Close();
+            }
+            catch (Exception) { return false; }
+            return true;
+        }
+        void OnStateChanged(object sender, EventArgs args)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                Hide();
+                if (notifyIcon != null)
+                    notifyIcon.ShowBalloonTip(2000);
+            }
+            else
+                m_storedWindowState = WindowState;
+        }
+        void OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            CheckTrayIcon();
+        }
+
+        void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = m_storedWindowState;
+        }
+        void CheckTrayIcon()
+        {
+            ShowTrayIcon(!IsVisible);
+        }
+
+        void ShowTrayIcon(bool show)
+        {
+            if (notifyIcon != null)
+                notifyIcon.Visible = show;
+        }
+        void OnClose(object sender, CancelEventArgs args)
+        {
+            notifyIcon.Dispose();
+            notifyIcon = null;
+        }
+
+        private void SetComboBoxValue()
+        {
+            switch (Properties.Settings.Default.Separator)
+            {
+                case "Enter":
+                    comboBox.SelectedIndex = 0;
+                    break;
+
+                case "Space":
+                    comboBox.SelectedIndex = 1;
+                    break;
+
+                case ", ":
+                    comboBox.SelectedIndex = 2;
+                    break;
+
+                case ". ":
+                    comboBox.SelectedIndex = 3;
+                    break;
+            }
+        }
+        private void OpenTelegram(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://t.me/CoderWorker");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!isInitializing)
+            {
+                Properties.Settings.Default.Separator = ((ComboBoxItem)comboBox.SelectedItem).Content.ToString();
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
